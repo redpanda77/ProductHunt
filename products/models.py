60be2b6527b3a5da775f67fb5936865a5e8e920e
Redpanda <gonzalocasajus@gmail.com>
2018-05-25 17:06:48 +0200

    product integration added

--- products/models.py
+++ products/models.py
@@ -8,9 +8,10 @@ class Product(models.Model):
     title       = models.CharField(max_length=255)
     pub_date    = models.DateTimeField()
     body        = models.TextField()
+    url        = models.TextField()
     image       = models.ImageField(upload_to='images/')
     icon        = models.ImageField(upload_to='images/')
-    image       = models.TextField()
+    image       = models.ImageField(upload_to='images/')
     votes_total = models.IntegerField(default=1)
     hunter      = models.ForeignKey(User, on_delete=models.CASCADE)
 
--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
+
 
 # Create your views here.
 def home(request):
@@ -7,4 +10,30 @@ def home(request):
 
 @login_required
 def create(request):
-    return render(request, 'products/create.html')
+    if request.method == 'POST':
+        print(request)
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
+            product = Product()
+            product.title = request.POST['title']
+            product.body = request.POST['body']
+            if request.POST['body'].startswith('http://') or request.POST['body'].startswith('https://'):
+                product.url = request.POST['url']
+            else:
+                product.url = 'http://' + request.POST['url']
+            product.body  = request.POST['body']
+            product.icon  = request.FILES['icon']
+            product.image = request.FILES['image']
+            product.pub_date = timezone.datetime.now()
+            product.votes_total = 1
+            product.hunter = request.user
+            product.save()
+            return redirect('/products/'+ str(product.id))
+
+        else:
+            return render(request, 'products/create.html', {'error':'All fields are required!'})
+    else:
+        return render(request, 'products/create.html')
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/detail.html', {'product':product})
