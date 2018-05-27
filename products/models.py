a3845b779e606e7d36a4badf3e86d6610cdc8b34
Redpanda <gonzalocasajus@gmail.com>
2018-05-27 19:17:58 +0200

    80% done

--- products/models.py
+++ products/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     title       = models.CharField(max_length=255)
     pub_date    = models.DateTimeField()
     body        = models.TextField()
-    url        = models.TextField()
+    url         = models.TextField()
     image       = models.ImageField(upload_to='images/')
     icon        = models.ImageField(upload_to='images/')
     image       = models.ImageField(upload_to='images/')
